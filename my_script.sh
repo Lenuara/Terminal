d83ab854887623a1d4e924e2dd0ec70403838e2e
Lenuara <keypost777@gmail.com>
2022-02-10 20:22:59 +0300

    HomeWork_1

---
+++ my_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is my first script!
+cd ~/LearningQA/Terminal/folder
+mkdir Folder_1 Folder_2 Folder_3
+cd Folder_1
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir Folder_a Folder_b Folder_c
+ls -la
+mv ./Folder_1/{file1.txt,file2.txt} ./Folder_2
